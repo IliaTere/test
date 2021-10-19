6ce8e1a188a96e66d8d8a61ad556716eb2261934
Ilia Tereshenko <@email>
2021-10-19 14:39:42 +0300

    1.3

--- 1.c
+++ 1.c
@@ -1,23 +1,3 @@
 #include <stdio.h>
-// test update 1.1
-int main (void){
-    double a,b;
-    double result = (scanf("%lf %lf", &a, &b));
-    if (result =! 2) {
-        printf ("Incorrect input\n");
-        return 2; 
-    }
-    if (a < b) {
-        printf("%lf", a);
-        return 0;   
-    }
-    else {
-        while (b == a) {
-            b = b - 1;
-            printf("%lf", b);
-        }
-        
-    }
-    return 0;
+// test update 1.3
 
-}
